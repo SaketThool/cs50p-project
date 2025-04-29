9bf85e4ecff30ddec1aa01f27bc15b0a3c5e2209
Saket <thoolsaket@gmail.com>
2025-04-29 14:54:42 +0000

    added routes

--- project.py
+++ project.py
@@ -4,22 +4,22 @@ def main():
 
     db_setup()
 
-    # figlet = pyfiglet.Figlet(font='slant')
-    # art = figlet.renderText("WELCOME")
-    # print(art)
+    figlet = pyfiglet.Figlet(font='slant')
+    art = figlet.renderText("WELCOME")
+    print(art)
 
-    # print("1. Book Your Train Ticket")
-    # print("2. Quit")
+    print("1. Book Your Train Ticket")
+    print("2. Quit")
 
-    # while True:
-    #     option = input("Enter the number you want choose\n")
-    #     match (option.lower()):
-    #         case "1":
-    #             print("Welcome to our website")
-    #             break
-    #         case "2":
-    #             print("Thank for visiting our website")
-    #             break
+    while True:
+        option = input("Enter the number you want choose\n")
+        match (option.lower()):
+            case "1":
+                print("Welcome to our website")
+                break
+            case "2":
+                print("Thank for visiting our website")
+                break
 
 
 
