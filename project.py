a948048607723ad96411541e6731b2f42eed5153
Saket <thoolsaket@gmail.com>
2025-05-05 15:03:05 +0000

    install black

--- project.py
+++ project.py
@@ -172,50 +172,84 @@ def choose_phone_number():
 
 def book_a_train(con):
     routes = get_all_routes(con)
-    
-    print("Here are all the arrival stations available")
+    if TEST:
+           
+        chosen_dep_station = "NGP"
 
-    departure_stations = set()
-    for route in routes:
-        departure_stations.add(route.dep_station)
-    departure_stations = sorted(departure_stations)
-    pprint(departure_stations) # TODO proper print
+        
 
-    chosen_dep_station = choose_dep_station(departure_stations)
+        chosen_arri_station = "CSMT"
+        
+        chosen_date = date.fromisoformat("2025-07-21")
 
-    arrival_stations = set()
-    for route in routes:
-        if chosen_dep_station == route.dep_station:
-            arrival_stations.add(route.arri_station)
-    arri_stations = sorted(arrival_stations)
-    pprint(arrival_stations) # TODO proper print , print full station name , use survey package
-
-    chosen_arri_station = choose_arri_station(arrival_stations)
-    
-    chosen_date = choose_date()
-
-    chosen_weekday = WEEKDAYS[chosen_date.weekday()]
+        chosen_weekday = WEEKDAYS[chosen_date.weekday()]
 
 
-    available_routes = []# TODO check the available seats
-    for route in routes:
-        if chosen_dep_station == route.dep_station and chosen_arri_station == route.arri_station and chosen_weekday in route.days_running:
-            available_routes.append(route)
-    if len(available_routes) == 0:
-        sys.exit("Trains are not available")
-    # TODO to print it nicely like pizza.py , print all information of train
-    for i,route in enumerate(available_routes):
-        print(f"{i}: {route}") # TODO print number of seats free
+        available_routes = []# TODO check the available seats
+        for route in routes:
+            if chosen_dep_station == route.dep_station and chosen_arri_station == route.arri_station and chosen_weekday in route.days_running:
+                available_routes.append(route)
+        if len(available_routes) == 0:
+            sys.exit("Trains are not available")
+        # TODO to print it nicely like pizza.py , print all information of train
+        for i,route in enumerate(available_routes):
+            print(f"{i}: {route}") # TODO print number of seats free
 
-    chosen_route_number = choose_route_number(available_routes)
+        chosen_route_number = choose_route_number(available_routes)
 
-    print(chosen_route_number)## TODO name, age, gender, ph. number
-    chosen_route = available_routes[chosen_route_number]
+        print(chosen_route_number)## TODO name, age, gender, ph. number
+        chosen_route = available_routes[chosen_route_number]
 
-    passanger_name = choose_name()
-    passanger_age = choose_age()
-    passanger_gender = choose_gender()
-    passanger_phone_number = choose_phone_number()
+        passanger_name = "saket"
+        passanger_age = 17
+        passanger_gender = "M"
+        passanger_phone_number = "9988776655"
+
+    else:
+
+        print("Here are all the arrival stations available")
+
+        departure_stations = set()
+        for route in routes:
+            departure_stations.add(route.dep_station)
+        departure_stations = sorted(departure_stations)
+        pprint(departure_stations) # TODO proper print
+
+        chosen_dep_station = choose_dep_station(departure_stations)
+
+        arrival_stations = set()
+        for route in routes:
+            if chosen_dep_station == route.dep_station:
+                arrival_stations.add(route.arri_station)
+        arri_stations = sorted(arrival_stations)
+        pprint(arrival_stations) # TODO proper print , print full station name , use survey package
+
+        chosen_arri_station = choose_arri_station(arrival_stations)
+        
+        chosen_date = choose_date()
+
+        chosen_weekday = WEEKDAYS[chosen_date.weekday()]
+
+
+        available_routes = []# TODO check the available seats
+        for route in routes:
+            if chosen_dep_station == route.dep_station and chosen_arri_station == route.arri_station and chosen_weekday in route.days_running:
+                available_routes.append(route)
+        if len(available_routes) == 0:
+            sys.exit("Trains are not available")
+        # TODO to print it nicely like pizza.py , print all information of train
+        for i,route in enumerate(available_routes):
+            print(f"{i}: {route}") # TODO print number of seats free
+
+        chosen_route_number = choose_route_number(available_routes)
+
+        print(chosen_route_number)## TODO name, age, gender, ph. number
+        chosen_route = available_routes[chosen_route_number]
+
+        passanger_name = choose_name()
+        passanger_age = choose_age()
+        passanger_gender = choose_gender()
+        passanger_phone_number = choose_phone_number()
 
 
 if __name__ == "__main__":
